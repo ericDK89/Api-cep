0a9cd6c067fd7746fb0bff90cf56f3178f393bf2
Eric Macedo <ericthr42@gmail.com>
2022-04-16 20:54:49 -0300

    Cep

---
+++ src/components/Cep.js
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+
+export default function Cep() {
+
+    const [api, setApi] = useState([])
+    const [cep, setCep] = useState('')
+
+    // useEffect(() => {
+    //     let url = `https://cep.awesomeapi.com.br/:format/${cep}`
+    //      axios.get(url)
+    //         .then(res => {setApi(res.data)})
+    // })
+
+    const cepApi = async () => {
+        let url = `https://cep.awesomeapi.com.br/:format/${cep}`
+        try {
+            const res = await axios.get(url)
+                return setApi(res.data)
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    useEffect(() => {
+        cepApi()
+    })
+
+    const handleInput = (event) => {
+        setCep(event.target.value)
+    }
+
+  return (
+    <div>
+        <form>
+            <input value={cep} onChange={handleInput} />
+        </form>
+        <ul>
+            <li> {api.address} </li>
+            <li> {api.state} </li>
+            <li> {api.district} </li>
+        </ul>
+    </div>
+  )
+}
